dee742340fa237a57895d37225122276ca06b1b4
Mal <anityakumala@gmail.com>
2025-03-17 13:08:20 +0700

    Create output.py

---
+++ Day-3/control flow and logical operator/output.py
@@ -0,0 +1,21 @@
+Hasil untuk Andi: Anda mendapatkan tunjangan kesehatan dan tunjangan transportasi.
+Hasil untuk Budi: Anda tidak mendapatkan tunjangan.
+Hasil untuk Cindy: Anda mendapatkan tunjangan kesehatan.
+Hasil untuk Doni: Anda tidak mendapatkan tunjangan.
+Hasil untuk Eka: Anda mendapatkan tunjangan kesehatan dan tunjangan transportasi.
+Hasil untuk Fani: Anda tidak mendapatkan tunjangan.
+Hasil untuk Gina: Anda mendapatkan tunjangan kesehatan.
+Hasil untuk Hadi: Anda mendapatkan tunjangan transportasi.
+Hasil untuk Ika: Anda tidak mendapatkan tunjangan.
+Hasil untuk Joko: Anda mendapatkan tunjangan kesehatan.
+Hasil untuk Kiki: Anda mendapatkan tunjangan kesehatan dan tunjangan transportasi.
+Hasil untuk Lina: Anda tidak mendapatkan tunjangan.
+Hasil untuk Maya: Anda mendapatkan tunjangan kesehatan.
+Hasil untuk Nina: Anda tidak mendapatkan tunjangan.
+Hasil untuk Omar: Anda mendapatkan tunjangan kesehatan dan tunjangan transportasi.
+Hasil untuk Pia: Anda tidak mendapatkan tunjangan.
+Hasil untuk Qori: Anda tidak mendapatkan tunjangan.
+Hasil untuk Rudi: Anda mendapatkan tunjangan kesehatan.
+Hasil untuk Sari: Anda mendapatkan tunjangan transportasi.
+Hasil untuk Tina: Anda tidak mendapatkan tunjangan.
+Hasil untuk Udin: Anda mendapatkan tunjangan kesehatan.
