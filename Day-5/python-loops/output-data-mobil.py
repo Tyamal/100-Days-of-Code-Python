fe1274e716a639d7e416cab77e1dd39344d1ac12
Mal <anityakumala@gmail.com>
2025-03-18 19:56:24 +0700

    Create output-data-mobil.py

---
+++ Day-5/python-loops/output-data-mobil.py
@@ -0,0 +1,40 @@
+ID Mobil: 1
+Nama Mobil: Toyota Camry
+Merek: Toyota
+Tahun Pembuatan: 2020
+Harga: 350000000 IDR
+Warna: Hitam
+Jenis Bahan Bakar: Bensin
+----------------------------------------
+ID Mobil: 2
+Nama Mobil: Honda Civic
+Merek: Honda
+Tahun Pembuatan: 2021
+Harga: 400000000 IDR
+Warna: Putih
+Jenis Bahan Bakar: Bensin
+----------------------------------------
+ID Mobil: 3
+Nama Mobil: Suzuki Ertiga
+Merek: Suzuki
+Tahun Pembuatan: 2019
+Harga: 250000000 IDR
+Warna: Merah
+Jenis Bahan Bakar: Bensin
+----------------------------------------
+ID Mobil: 4
+Nama Mobil: Daihatsu Xenia
+Merek: Daihatsu
+Tahun Pembuatan: 2020
+Harga: 230000000 IDR
+Warna: Biru
+Jenis Bahan Bakar: Bensin
+----------------------------------------
+ID Mobil: 5
+Nama Mobil: Nissan Leaf
+Merek: Nissan
+Tahun Pembuatan: 2022
+Harga: 600000000 IDR
+Warna: Hijau
+Jenis Bahan Bakar: Listrik
+----------------------------------------
