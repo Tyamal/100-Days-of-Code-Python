c65f26af8fc01763c03946678b4d7c5bced04f7e
Mal <anityakumala@gmail.com>
2025-03-18 20:52:19 +0700

    Update viz-karel.py

--- Day-6/karel/viz-karel.py
+++ Day-6/karel/viz-karel.py
@@ -1,19 +1,15 @@
 def visualize_karel_world(positions, beeper_counts):
-    # Membuat visualisasi dunia Karel
     world = []
     for pos, count in zip(positions, beeper_counts):
         if count > 0:
-            world.append("B")  # B untuk beeper
+            world.append("B")
         else:
-            world.append("-")  # - untuk posisi kosong
+            world.append("-")
 
-    # Menampilkan visualisasi
     print("Posisi: ", " ".join(map(str, positions)))
     print("Beeper: ", " ".join(world))
 
-# Data posisi dan jumlah beeper
 positions = list(range(1, 11))
 beeper_counts = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
 
-# Menjalankan fungsi untuk visualisasi
 visualize_karel_world(positions, beeper_counts)
