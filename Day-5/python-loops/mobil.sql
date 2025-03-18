5b75dad7299e5304248ab1916803cea9266c8477
Mal <anityakumala@gmail.com>
2025-03-18 19:58:00 +0700

    Create mobil.sql

---
+++ Day-5/python-loops/mobil.sql
@@ -0,0 +1,6 @@
+INSERT INTO Mobil (nama_mobil, merek, tahun_pembuatan, harga, warna, jenis_bahan_bakar) VALUES
+('Toyota Camry', 'Toyota', 2020, 350000000, 'Hitam', 'Bensin'),
+('Honda Civic', 'Honda', 2021, 400000000, 'Putih', 'Bensin'),
+('Suzuki Ertiga', 'Suzuki', 2019, 250000000, 'Merah', 'Bensin'),
+('Daihatsu Xenia', 'Daihatsu', 2020, 230000000, 'Biru', 'Bensin'),
+('Nissan Leaf', 'Nissan', 2022, 600000000, 'Hijau', 'Listrik');
