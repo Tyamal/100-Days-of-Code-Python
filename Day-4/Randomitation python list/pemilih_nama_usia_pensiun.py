bbe830c105692cbda7b62e0d3fd16c5e3634b3fb
Mal <anityakumala@gmail.com>
2025-03-18 11:16:10 +0700

    Update pemilih_nama_usia_pensiun.py

--- Day-4/Randomitation python list/pemilih_nama_usia_pensiun.py
+++ Day-4/Randomitation python list/pemilih_nama_usia_pensiun.py
@@ -12,4 +12,32 @@ def hitung_masa_pensiun(umur):
 def pilih_karyawan(data):
     karyawan_valid = []
     for karyawan in data:
-        id_karyawan,
+        id_karyawan, nama, alamat, gaji, umur, lama_bekerja, posisi = karyawan
+        umur = int(umur)
+        lama_bekerja = int(lama_bekerja)
+        gaji = int(gaji)
+        
+        masa_pensiun = hitung_masa_pensiun(umur)
+        
+        if masa_pensiun > 0:
+            karyawan_valid.append(karyawan)
+
+    jabatan_urut = {
+        'Manager': 1,
+        'Supervisor': 2,
+        'Staff': 3
+    }
+    
+    karyawan_valid.sort(key=lambda x: (jabatan_urut[x[6]], -int(x[3]), -int(x[5])))
+
+    return karyawan_valid
+
+data_kepegawaian = baca_data_kepegawaian('data_kepegawaian.txt')
+karyawan_terpilih = pilih_karyawan(data_kepegawaian)
+
+if karyawan_terpilih:
+    karyawan_acak = random.choice(karyawan_terpilih)
+    print("Karyawan yang dipilih secara acak berdasarkan kriteria adalah:")
+    print(f"ID: {karyawan_acak[0]}, Nama: {karyawan_acak[1]}, Alamat: {karyawan_acak[2]}, Gaji: {karyawan_acak[3]}, Umur: {karyawan_acak[4]}, Lama Bekerja: {karyawan_acak[5]}, Posisi: {karyawan_acak[6]}")
+else:
+    print("Tidak ada karyawan yang memenuhi kriteria.")
