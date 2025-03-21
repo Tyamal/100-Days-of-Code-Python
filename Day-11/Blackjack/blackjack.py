51a334685ecec62a5ad48b7704b3530985175921
Mal <anityakumala@gmail.com>
2025-03-21 13:28:15 +0700

    Update blackjack.py

--- Day-11/Blackjack/blackjack.py
+++ Day-11/Blackjack/blackjack.py
@@ -19,3 +19,7 @@ class Card:
 
 # Kelas untuk dek
 class Deck:
+    def __init__(self):
+        self.cards = [Card(suit, rank) for suit in ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+                      for rank in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']]
+        random.shuffle(self.cards)
