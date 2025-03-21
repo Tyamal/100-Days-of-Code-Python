4afaf5fa1187cf7a3027a57690ba39ed7c75a660
Mal <anityakumala@gmail.com>
2025-03-21 13:26:27 +0700

    Update blackjack.py

--- Day-11/Blackjack/blackjack.py
+++ Day-11/Blackjack/blackjack.py
@@ -5,3 +5,9 @@ class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+
+    def value(self):
+        if self.rank in ['J', 'Q', 'K']:
+            return 10
+        elif self.rank == 'A':
+            return 11  # Aces can be 1 or 11, handled later
